95bd9a9ff967498027b4cbca3cc9d7f4bba88bed
Yuri Sartini Coelho <137238970+yurisartini120@users.noreply.github.com>
2023-10-30 15:46:06 -0300

    finish

--- Desenvolvimento-mobile/Calculadora/app/src/main/java/devandroid/yuri/calculadora/view/MainActivity.java
+++ Desenvolvimento-mobile/Calculadora/app/src/main/java/devandroid/yuri/calculadora/view/MainActivity.java
@@ -11,54 +11,39 @@ import android.widget.Toast;
 import devandroid.yuri.calculadora.R;
 
 public class MainActivity extends AppCompatActivity {
-
     EditText numero1EditText;
     EditText numero2EditText;
-
     Button adicaoButton;
     Button subtracaoButton;
     Button multiplicacaoButton;
     Button divisaoButton;
-
     TextView resultadoTextView;
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vincularComponentes();
+        criarListeners();
     }
-
-    private void vincularComponentes(){
-
+    private void vincularComponentes() {
         numero1EditText = findViewById(R.id.numero1EditText);
         numero2EditText = findViewById(R.id.numero2EditText);
-
         adicaoButton = findViewById(R.id.adicaoButton);
         subtracaoButton = findViewById(R.id.subtracaoButton);
         multiplicacaoButton = findViewById(R.id.multiplicacaoButton);
         divisaoButton = findViewById(R.id.divisaoButton);
-
         resultadoTextView = findViewById(R.id.resultadoTextView);
-
-
     }
-
     private void criarListeners() {
-
         adicaoButton.setOnClickListener(evt -> adicionar());
         subtracaoButton.setOnClickListener(evt -> subtrair());
         multiplicacaoButton.setOnClickListener(evt -> multiplicar());
         divisaoButton.setOnClickListener(evt -> dividir());
-
     }
     private void adicionar() {
 
         String numero1String = numero1EditText.getText().toString();
         String numero2String = numero2EditText.getText().toString();
-
         if (numero1String.isEmpty() || numero2String.isEmpty()) {
             Toast.makeText(this, "Digite dois números",
                     Toast.LENGTH_LONG).show();
@@ -71,14 +56,10 @@ public class MainActivity extends AppCompatActivity {
         double resultado = numero1 + numero2;
 
         resultadoTextView.setText(String.valueOf(resultado));
-
-
     }
     private void subtrair() {
-
         String numero1String = numero1EditText.getText().toString();
         String numero2String = numero2EditText.getText().toString();
-
         if (numero1String.isEmpty() || numero2String.isEmpty()) {
             Toast.makeText(this, "Digite dois números",
                     Toast.LENGTH_LONG).show();
@@ -91,16 +72,10 @@ public class MainActivity extends AppCompatActivity {
         double resultado = numero1 - numero2;
 
         resultadoTextView.setText(String.valueOf(resultado));
-
-
-
     }
     private void multiplicar() {
-
         String numero1String = numero1EditText.getText().toString();
         String numero2String = numero2EditText.getText().toString();
-
-
         if (numero1String.isEmpty() || numero2String.isEmpty()) {
             Toast.makeText(this, "Digite dois números",
                     Toast.LENGTH_LONG).show();
@@ -113,25 +88,17 @@ public class MainActivity extends AppCompatActivity {
         double resultado = numero1 * numero2;
 
         resultadoTextView.setText(String.valueOf(resultado));
-
-
     }
     private void dividir() {
-
         String numero1String = numero1EditText.getText().toString();
         String numero2String = numero2EditText.getText().toString();
-
-
         if (numero1String.isEmpty() || numero2String.isEmpty()) {
             Toast.makeText(this, "Digite dois números",
                     Toast.LENGTH_LONG).show();
             return;
         }
-
         double numero1 = Double.parseDouble(numero1String);
         double numero2 = Double.parseDouble(numero2String);
-
-
         if (numero2 == 0) {
             Toast.makeText(this, "Não é possível dividir por 0",
                     Toast.LENGTH_LONG).show();
@@ -140,7 +107,5 @@ public class MainActivity extends AppCompatActivity {
 
         double resultado = numero1 / numero2;
         resultadoTextView.setText(String.valueOf(resultado));
-
-
     }
 }
