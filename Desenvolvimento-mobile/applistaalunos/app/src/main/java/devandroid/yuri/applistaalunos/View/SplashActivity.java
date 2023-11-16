36678c9b4eb1835cf941d641637d6f55f6c2ea2b
Yuri Sartini Coelho <137238970+yurisartini120@users.noreply.github.com>
2023-11-16 16:38:51 -0300

    l

--- Desenvolvimento-mobile/applistaalunos/app/src/main/java/devandroid/yuri/applistaalunos/View/SplashActivity.java
+++ Desenvolvimento-mobile/applistaalunos/app/src/main/java/devandroid/yuri/applistaalunos/View/SplashActivity.java
@@ -18,6 +18,7 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
