1f7fdc04370a2f3b8b672c0fdbfba708c20e9d9e
Yuri Sartini Coelho <137238970+yurisartini120@users.noreply.github.com>
2023-11-29 16:29:14 -0300

    botão voltar configurado

---
+++ Desenvolvimento-mobile/SkillStash/app/src/main/java/devandroid/yuri/skillstash/AdicionarCursoActivity.java
@@ -0,0 +1,31 @@
+package devandroid.yuri.skillstash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AdicionarCursoActivity extends AppCompatActivity {
+
+    Button btnVoltar;
+    Button btnSalvar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_curso);
+
+        btnVoltar = findViewById(R.id.btnVoltar);
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+
+}
--- Desenvolvimento-mobile/SkillStash/app/src/main/java/devandroid/yuri/skillstash/view/MainActivity.java
+++ Desenvolvimento-mobile/SkillStash/app/src/main/java/devandroid/yuri/skillstash/view/MainActivity.java
@@ -2,9 +2,12 @@ package devandroid.yuri.skillstash.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import devandroid.yuri.skillstash.AdicionarCursoActivity;
 import devandroid.yuri.skillstash.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
         btnAdicionar = findViewById(R.id.btnAdicionar);
         btnDeletar = findViewById(R.id.btnDeletar);
 
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, AdicionarCursoActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
 
 
