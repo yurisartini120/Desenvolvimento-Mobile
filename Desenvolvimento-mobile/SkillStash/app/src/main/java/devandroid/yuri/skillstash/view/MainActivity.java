ecf928033f58b0150115fc0481627035bbe4a3ab
Yuri Sartini Coelho <137238970+yurisartini120@users.noreply.github.com>
2023-11-30 15:21:43 -0300

    fiz o do python

--- Desenvolvimento-mobile/SkillStash/app/src/main/java/devandroid/yuri/skillstash/view/MainActivity.java
+++ Desenvolvimento-mobile/SkillStash/app/src/main/java/devandroid/yuri/skillstash/view/MainActivity.java
@@ -79,6 +79,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        imageButtonPython.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PythonActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
