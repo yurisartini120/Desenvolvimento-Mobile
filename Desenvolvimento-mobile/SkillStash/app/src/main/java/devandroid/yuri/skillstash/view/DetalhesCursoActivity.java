ddefc26e298b62b8511630477604ca084e9afc19
Yuri Sartini Coelho <137238970+yurisartini120@users.noreply.github.com>
2023-12-01 14:50:33 -0300

    voltar funcionando no detalhes

--- Desenvolvimento-mobile/SkillStash/app/src/main/java/devandroid/yuri/skillstash/view/DetalhesCursoActivity.java
+++ Desenvolvimento-mobile/SkillStash/app/src/main/java/devandroid/yuri/skillstash/view/DetalhesCursoActivity.java
@@ -3,6 +3,8 @@ package devandroid.yuri.skillstash.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import devandroid.yuri.skillstash.R;
@@ -10,10 +12,25 @@ import devandroid.yuri.skillstash.model.Curso;
 
 public class DetalhesCursoActivity extends AppCompatActivity {
 
+    Button btnVoltarDetalhes;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes_curso);
+
+        btnVoltarDetalhes = findViewById(R.id.btnVoltarDetalhes);
+
+        btnVoltarDetalhes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
 
         if (getIntent().hasExtra("curso")){
 
